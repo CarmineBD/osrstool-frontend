b0ed2ce691857593ba210f05f9b36a3a61ad8a08
Carmine Buonaiuto <carmine-bnt@hotmail.com>
2025-08-05 20:46:36 +0200

    feat: llamada inicial de datos en landing

--- src/features/methods/hooks.ts
+++ src/features/methods/hooks.ts
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchMethods } from "../../lib/api";
 
-export function useMethods(username: string) {
+export function useMethods(username?: string) {
   return useQuery({
     queryKey: ["methods", username],
     queryFn: () => fetchMethods(username),
     refetchInterval: 60 * 1000,
     staleTime: 30 * 1000,
-    enabled: !!username,
     retry: false,
   });
 }
