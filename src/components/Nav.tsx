dbaaf9a4d634caedd7fc4eafba554a3540876438
Carmine Buonaiuto <carmine-bnt@hotmail.com>
2025-08-26 17:57:19 +0200

    feat: navigation menu mock

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,8 +4,57 @@ import { useQueryClient } from "@tanstack/react-query";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useUsername } from "@/contexts/UsernameContext";
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 export type Props = { hideInput?: boolean };
+
+import * as React from "react";
+import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Alert Dialog",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Hover Card",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Progress",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Scroll-area",
+    href: "/docs/primitives/scroll-area",
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "Tabs",
+    href: "/docs/primitives/tabs",
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "Tooltip",
+    href: "/docs/primitives/tooltip",
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
 
 export function Nav({ hideInput }: Props) {
   const { username, setUsername, userError, setUserError } = useUsername();
@@ -27,7 +76,9 @@ export function Nav({ hideInput }: Props) {
           if (location.pathname === "/") {
             queryClient.invalidateQueries({ queryKey: ["methods", username] });
           } else if (id) {
-            queryClient.invalidateQueries({ queryKey: ["methodDetail", id, username] });
+            queryClient.invalidateQueries({
+              queryKey: ["methodDetail", id, username],
+            });
             queryClient.invalidateQueries({ queryKey: ["items"] });
             queryClient.invalidateQueries({ queryKey: ["variantHistory"] });
           }
@@ -46,10 +97,130 @@ export function Nav({ hideInput }: Props) {
   };
 
   return (
-    <nav className="flex items-center justify-between p-4 bg-white shadow">
+    <nav className="flex items-center justify-between p-4 bg-white shadow z-10">
       <Link to="/" className="flex items-center space-x-2">
-        <img src="/logo.png" alt="Logo" className="h-8 w-auto" />
+        <img
+          src="https://oldschool.runescape.wiki/images/thumb/Coins_detail.png/120px-Coins_detail.png?404bc"
+          alt="Logo"
+          className="h-8 w-auto"
+        />
+        <span className="text-xl font-bold">GP Now</span>
       </Link>
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Money making methods</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <a
+                      className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
+                      href="/"
+                    >
+                      <div className="mt-4 mb-2 text-lg font-medium">
+                        All methods
+                      </div>
+                      <p className="text-muted-foreground text-sm leading-tight">
+                        All aviable money making methods on OSRS
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href="/docs" title="Hottest">
+                  Money making methods that are trending now.
+                </ListItem>
+                <ListItem href="/docs/installation" title="Most favourites">
+                  Money making methods that have been favourited the most by
+                  users.
+                </ListItem>
+                <ListItem
+                  href="/docs/primitives/typography"
+                  title="Most viable"
+                >
+                  Money making methods that are the most profitable.
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Training methods</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Calculators</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[300px] gap-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">
+                      <div className="font-medium">Skilkling Calculators</div>
+                      <div className="text-muted-foreground">
+                        Focused on terms of skilling
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">
+                      <div className="font-medium">
+                        Money making calculators
+                      </div>
+                      <div className="text-muted-foreground">
+                        Focused on making money
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Useful tables</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[300px] gap-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">
+                      <div className="font-medium">Components</div>
+                      <div className="text-muted-foreground">
+                        Browse all components in the library.
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">
+                      <div className="font-medium">Documentation</div>
+                      <div className="text-muted-foreground">
+                        Learn how to use the library.
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink asChild>
+                    <Link href="#">
+                      <div className="font-medium">Blog</div>
+                      <div className="text-muted-foreground">
+                        Read our latest blog posts.
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <div className="flex items-center gap-4">
         {!hideInput && (
           <div className="flex flex-col gap-1">
@@ -62,9 +233,7 @@ export function Nav({ hideInput }: Props) {
               />
               <Button type="submit">Buscar</Button>
             </form>
-            {userError && (
-              <p className="text-red-500 text-sm">{userError}</p>
-            )}
+            {userError && <p className="text-red-500 text-sm">{userError}</p>}
           </div>
         )}
         <span className="text-sm text-gray-500">
@@ -72,5 +241,25 @@ export function Nav({ hideInput }: Props) {
         </span>
       </div>
     </nav>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="text-sm leading-none font-medium">{title}</div>
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
   );
 }
